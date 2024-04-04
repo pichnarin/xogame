c5dc3f1a7bdaf3fdcbcef55d577a7426adfcd04a
noobmaster69 <tleanghour67@gmail.com>
2024-04-04 09:07:30 +0700

    on going project

--- src/main/java/com/game/tictactoe/Application.java
+++ src/main/java/com/game/tictactoe/Application.java
@@ -1,18 +1,20 @@
 package com.game.tictactoe;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("startUpITF.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("tictactoe");
+        stage.centerOnScreen();
+        stage.getIcons().add(new Image("https://cdn-icons-png.flaticon.com/512/566/566294.png"));
         stage.setScene(scene);
         stage.show();
     }
